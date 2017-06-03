27d226e431e1f5214cc360601cbc436af227aa50
zhuangqh <zhuangqhc@163.com>
2017-06-03 22:58:00 +0800

    完善service里的请求

--- src/service/index.js
+++ src/service/index.js
@@ -11,7 +11,7 @@ const baseUrl = ''
  * @constructor
  */
 const CRUD = (ctx, operation, fakeUrl, form) => {
-  const url = baseUrl + '/api/' + fakeUrl
+  const url = baseUrl + '/api' + fakeUrl
 
   const handleResponse = res => {
     if (res.body && res.body.status === '0') {
@@ -31,15 +31,57 @@ const CRUD = (ctx, operation, fakeUrl, form) => {
 
 const User = {
   login (ctx, user) {
-    return CRUD(ctx, 'post', 'user/login', user)
+    return CRUD(ctx, 'post', '/user/login', user)
   },
 
   logout (ctx) {
-    return CRUD(ctx, 'get', `user/logout`)
+    return CRUD(ctx, 'get', `/user/logout`)
   },
 
   register (ctx, user) {
-    return CRUD(ctx, 'post', 'user/register', user)
+    return CRUD(ctx, 'post', '/user/register', user)
+  }
+}
+
+const Movie = {
+  fetch (ctx) {
+    return CRUD(ctx, 'get', '/movie/hotMovies')
+  },
+
+  fetchOne (ctx, id) {
+    return CRUD(ctx, 'get', `/movie/detail/${id}`)
+  },
+}
+
+const Order = {
+  fetch (ctx) {
+    return CRUD(ctx, 'get', '/order')
+  },
+
+  create (ctx, order) {
+    return CRUD(ctx, 'post', '/order', order)
+  },
+
+  remove (ctx, id) {
+    return CRUD(ctx, 'delete', `/order/${id}`)
+  },
+
+  fetchOne (ctx, id) {
+    return CRUD(ctx, 'get', `/order/${id}`)
+  },
+
+  pay (ctx, id) {
+    return CRUD(ctx, 'post', `/order/${id}`)
+  }
+}
+
+const Yueyin = {
+  fetch (ctx) {
+    return CRUD(ctx, 'get', '/yueyin')
+  },
+
+  create (ctx) {
+    return CRUD(ctx, 'post', '/yueyin')
   }
 }
 
@@ -53,11 +95,15 @@ const HTTPErrHandler = (ctx, err) => {
   } else if (err.status === 200) {
     bus.$emit('notify', err.body.msg)
   } else {
+    console.log(err)
     bus.$emit('notify', '网络错误')
   }
 }
 
 export {
   User,
+  Order,
+  Yueyin,
+  Movie,
   HTTPErrHandler
 }
