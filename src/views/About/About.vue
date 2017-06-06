0ff6230a96c826d34f291339a3f68ae6fe7ace80
zhuangqh <zhuangqhc@163.com>
2017-06-06 21:23:34 +0800

    可上传用户头像

--- src/views/About/About.vue
+++ src/views/About/About.vue
@@ -7,19 +7,20 @@
     <div class="main">
       <div class="avatar">
         <mu-paper class="demo-paper" circle :zDepth="2">
-          <div class="userImg">
+          <img class="uploadedImg" v-if="user.image" :src="user.image">
             <el-upload
-              class="upload-demo"
-              action="http://chuantu.biz/upload.php"
+              action="/api/user/image"
+              name="image"
               :show-file-list="false"
               :on-progress="handleProgress"
-              :on-success="handleSuccess">
-              <img class="uploadedImg" v-if="imageUrl" :src="imageUrl">
-              <el-button v-else size="small" :loading="loading">头像 <i class="el-icon-upload el-icon&#45;&#45;right"></i>
+              :on-error="handleUploadError"
+              :on-success="handleSuccess"
+              :before-upload="beforeUpload">
+              <el-button v-if="!user.image" size="small" :loading="loading">头像 <i class="el-icon-upload el-icon&#45;&#45;right"></i>
               </el-button>
             </el-upload>
-          </div>
         </mu-paper>
+        <p class="center">{{ user.username }}</p>
       </div>
 
       <mu-list>
@@ -37,45 +38,86 @@
           <mu-icon slot="right" value="chevron_right"/>
         </mu-list-item>
       </mu-list>
-      <div class="about_login">
-        <mu-raised-button label="登录" @click="login" primary></mu-raised-button>
-      </div>
     </div>
 
   </div>
 </template>
 
 <script>
+  import { User, HTTPErrHandler } from '../../service'
+  import bus from '../../service/bus'
+
   export default {
     data() {
       return {
-        userImgExisted: true,
+        username: 'unknown',
+
         loading: false,
-        imageUrl: '/static/images/fuck.png'
+        user: {
+          email: '',
+          image: '',
+          phone: '',
+          username: ''
+        }
       }
     },
+    created () {
+      this.getUserInfo()
+    },
     methods: {
-      login () {
-        this.$router.push('/login')
+      getUserInfo () {
+        User.me(this)
+          .then(res => {
+            this.user = res.body.data
+          })
+          .catch(err => {
+            HTTPErrHandler(this, err)
+          })
       },
       handleProgress(event, file, fileList) {
-        this.loading = true;
+        this.loading = true
       },
-      handleSuccess(response, file) {
-        this.userImgExisted = false;
-        this.imageUrl = URL.createObjectURL(file.raw);
+
+      handleUploadError (err, file, fileList) {
+        this.notify('上传失败')
+      },
+      handleSuccess (res, file) {
+        this.loading = false
+        this.user.image = res.data
+        this.notify('上传成功')
+        User.update(this, this.user)
+          .then(() => {
+            this.notify('上传头像成功')
+          })
+          .catch(err => {
+            HTTPErrHandler(this, err)
+          })
+      },
+
+      beforeUpload (file) {
+        const isImage = ['image/jpeg', 'image/jpg', 'image/png'].indexOf(file.type) !== -1
+        const isLt = file.size / 1024 / 1024 < 5
+
+        if (!isImage) {
+          this.$message.error('上传头像图片只能是 JPG 格式!')
+        }
+        if (!isLt) {
+          this.$message.error('上传头像图片大小不能超过 5MB!')
+        }
+        return isImage && isLt
+      },
+
+      notify (msg) {
+        bus.$emit('notify', msg)
       }
     }
   }
 </script>
 
 <style scoped>
-  .about {
-  }
 
-  .about_login {
+  .center {
     text-align: center;
-    padding-top: 100px;
   }
 
   .avatar {
@@ -84,14 +126,12 @@
     padding-top: 40px;
   }
 
-  .userImg {
-  }
-
   .uploadedImg {
-    height: 70px;
-    width: 70px;
+    height: 100%;
+    width: 100%;
     vertical-align: middle;
     text-align: center;
+    border-radius: 50%;
   }
 
   .demo-paper {
